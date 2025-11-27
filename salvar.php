b2c462974e95b98f1d8f52f96a9e85a9bcd3a094
Ricardo <ricardaoquadros@gmail.com>
2025-11-27 13:53:39 -0300

    phperro

--- salvar.php
+++ salvar.php
@@ -2,6 +2,13 @@
 /** -------------------------------------------------------
  *  SALVAR.PHP – CPF / MATRÍCULA + TELEFONE FORMATADOS
  * -------------------------------------------------------- */
+error_log("TESTE DE LOG SALVAR.PHP EXECUTADO");
+ini_set('display_errors', 0);
+ini_set('display_startup_errors', 0);
+error_reporting(E_ALL);
+
+ini_set('log_errors', 1);
+ini_set('error_log', __DIR__ . '/logs_php_errors.log');
 
 define('APP_RUNNING', true);
 
